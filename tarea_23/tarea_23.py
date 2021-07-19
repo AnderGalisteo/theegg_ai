cedd260c4b73ca46d7bc254d36d727c2160290ce
AnderGalisteo <andergalisteo@gmail.com>
2021-07-20 00:44:20 +0200

    WIP

--- tarea_23/tarea_23.py
+++ tarea_23/tarea_23.py
@@ -31,30 +31,86 @@ Por ejemplo, para cifrar el primer mensaje mencionado en la novela de Stephenson
 
 Si eres realmente bueno, puedes aprender a sumar letras en tu cabeza, y simplemente sumar las letras del paso (1) y (2). Sólo hace falta un poco de práctica. Es fácil recordar que A+A=B; recordar que T+Q=K es más difícil. 
 """
-def generar_solitario(tamano):
-    return "A"*tamano
+import random
+
+
+def generar_solitario(tamano,baraja):
+    num_itera = 0
+    solitario = []
+    while (num_itera<tamano):
+        index_comodin_0 = baraja.index(0)
+        if index_comodin_0 == 53:
+            baraja = list(baraja[0])+list(baraja[53])+list(baraja[1:53])
+        else:
+            tmp = baraja[index_comodin_0]
+            baraja[index_comodin_0] = baraja[index_comodin_0+1]
+            baraja[index_comodin_0+1] = tmp
+        index_comodin_1 = baraja.index(1)
+        if index_comodin_1 == 53:
+            baraja = list(baraja[0:2])+list(baraja[53])+list(baraja[2:53])
+        elif index_comodin_1 == 52:
+            baraja = list(baraja[0])+list(baraja[52])+list(baraja[1:52])+list(baraja[53])
+        else:
+            tmp = baraja[index_comodin_0]
+            baraja[index_comodin_0] = baraja[index_comodin_0+1]
+            baraja[index_comodin_0+1] = tmp
+        index_comodin_0 = baraja.index(0)
+        index_comodin_1 = baraja.index(1)
+        smaller_index = min(index_comodin_0,index_comodin_1)
+        higher_index = max(index_comodin_0,index_comodin_1)
+
+        baraja = list(baraja[higher_index+1:54])+list(baraja[smaller_index:higher_index+1])+list(baraja[0:smaller_index])
+        if baraja[53]!=0 and baraja[53]!=1:
+            value = baraja[53]-1
+            baraja = baraja[value+1:53]+baraja[0:value+1]+baraja[53:54]
+        if baraja[baraja[0]] == 0 or baraja[baraja[0]] == 1:
+            continue
+        if baraja[baraja[0]] > 26:
+            solitario.append(baraja[baraja[0]]-26)
+        else:
+            solitario.append(baraja[baraja[0]])
+        num_itera = num_itera+1
+    return ''.join([chr(i+64) for i in solitario])
 def divide_into_chunks(frase, tamano):
     chunks, chunk_size = len(frase), tamano
     splitted_frase = [ frase[i:i+chunk_size] for i in range(0, chunks, chunk_size) ]
     return splitted_frase
 def from_string_to_int_list(frase):
     return [int(i)-64 for i in list(map(str, map(ord, list(frase))))]
+def from_int_to_string(frase_lista):
+    return ''.join([chr(i+64) for i in frase_lista])
+def sum_listas(frase_lista, solitario_lista):
+    return [(frase_lista[i] + solitario_lista[i])%26 for i in range(len(frase_lista))]
+def res_listas(frase_lista, solitario_lista):
+    return [(frase_lista[i]+26 - solitario_lista[i])%26 for i in range(len(frase_lista))]
+    
 
-def cifrar(frase):
+
+def cifrar(frase,baraja):
     frase = frase.upper().replace(" ", "")
     if not(len(frase)%5 == 0):
         frase = frase + "X"*(5-len(frase)%5)
     #splitted_frase = divide_into_chunks(frase, 5)
-    solitario = generar_solitario(len(frase))
+    solitario = generar_solitario(len(frase),baraja)
     #splitted_solitario = divide_into_chunks(solitario, 5)
-    test_list = from_string_to_int_list(frase)
-    print(test_list)
+    frase_lista = from_string_to_int_list(frase)
+    solitario_lista = from_string_to_int_list(solitario)
+    suma_listas = sum_listas(frase_lista, solitario_lista)
+    return from_int_to_string(suma_listas)
 
-
+def descifrar(frase,baraja):
+    solitario = generar_solitario(len(frase),baraja)
+    frase_lista = from_string_to_int_list(frase)
+    solitario_lista = from_string_to_int_list(solitario)
+    resta_listas = res_listas(frase_lista, solitario_lista)
+    return from_int_to_string(resta_listas).rstrip("X") 
 
 
 if __name__ == "__main__":
-    frase = input("Introduzca la frase: ")
     
-    cifrar(frase)
+    baraja = list(range(0,54))
+    random.shuffle(baraja)
+
+    frase = input("Introduzca la frase: ")
+    print(descifrar(cifrar(frase,baraja),baraja))
 
