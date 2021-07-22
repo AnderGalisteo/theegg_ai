e93866a18d5b3e8b0d2df85a46a835aaa6febd7e
AnderGalisteo <andergalisteo@gmail.com>
2021-07-22 22:48:53 +0200

    Update 21 with comments

--- tarea_21/tarea_21.py
+++ tarea_21/tarea_21.py
@@ -1,3 +1,14 @@
+"""
+Según la definición de google un algoritmo es: "Un conjunto ordenado de operaciones sistemáticas que permite hacer un cálculo y 
+hallar la solución de un tipo de problemas".En esta tarea debes desarrollar (en el lenguaje o lenguajes de programación que tú 
+quieras) el siguiente algoritmo:Un programa que dado un número introducido entre 0,0001 y 0,9999 (no más de 4 cifras decimales), 
+obtenga y muestre la correspondiente fracción irreducible.Por ejemplo, el número 0,25 se puede obtener a partir de 25/100, o de 2/8, 
+o de 1/4, entre otros. La fracción irreducible es 1/4, que está formada por un numerador y un denominador que son primos entre sí.
+"""
+"""
+Lo he programado en python y el algoritmo es muy sencillo. Intenta dividir por 2 y luego por 3 etc hasta que encuentre un número por 
+el que dividirlo o llegue hasta el mínimo entre el numerador y el denominador.
+"""
 def verificar_y_calcular(val):
     try:
         x = float(val)
@@ -13,14 +24,14 @@ def verificar_y_calcular(val):
     numerador = int(x*10000)
     denominador = 10000
 
-    maxnum = max(numerador, denominador)
+    maxnum = min(numerador, denominador)
 
     i = 2
-    while(i<maxnum):
+    while(i<=maxnum):
         if numerador%i == 0 and denominador%i == 0:
             numerador = numerador/i
             denominador = denominador/i
-            maxnum = max(numerador, denominador)
+            maxnum = min(numerador, denominador)
             i = 2
         else:
             i = i + 1
